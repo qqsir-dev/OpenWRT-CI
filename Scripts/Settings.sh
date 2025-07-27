b816341f6556bfb0c871f7ecf779a9791bdc4e76
YG <54274847+qqsir-dev@users.noreply.github.com>
2025-07-27 17:03:05 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -30,7 +30,7 @@ if echo "$WRT_CONFIG" | grep -Eiq "ROCK"; then
  	sed -i "/exit 0/iuci add firewall redirect\nuci set firewall.@redirect[0].target=\'DNAT\'\nuci set firewall.@redirect[0].src=\'wan\'\nuci set firewall.@redirect[0].dest=\'lan\'\nuci set firewall.@redirect[0].proto=\'tcp udp\'\nuci set firewall.@redirect[0].src_dport=\'8098\'\nuci set firewall.@redirect[0].dest_ip=\'$WRT_IP\'\nuci set firewall.@redirect[0].dest_port=\'80\'\nuci set firewall.@redirect[0].name=\'Router\'\nuci commit firewall\n" $SET_NETWROK
   	echo "$WRT_CONFIG - $WRT_IP SET"
 fi
-cat "$SET_NETWROK"
+# cat "$SET_NETWROK"
 
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
